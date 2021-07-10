15c0b00b9731f35c66843da55ca9782b4b476617
Ricky <rnguyen89@gmail.com>
2021-07-10 00:24:50 -0700

    linked google font and added JS dataset

---
+++ main.js
@@ -0,0 +1,34 @@
+let pageNumber = 0
+
+const pages = [
+  {
+    copy: "Your garbage disposal is just a blender making disgusting smoothies.",
+    background: "url(/assets/image-1.jpg)",
+    circle: "#3e78ed"
+  },
+  
+  {
+    copy: "\“You deserve it\” sounds congratulatory. But \“You deserved it\” sounds aggressive",
+    background: "url(/assets/image-2.jpg)",
+    circle: "#3e78ed"
+  },
+
+  {
+    copy: "Unless you do it naked, you are never 100% done with laundry.",
+    background: "url(/assets/image-3.jpg)",
+    circle: "#3e78ed"
+  },
+
+  {
+    copy: "Your handwriting is just your own personalized font.",
+    background: "url(/assets/image-4.jpg)",
+    circle: "#3e78ed"
+  },
+
+  {
+    copy: "Resting your eyes in the morning feels more like sleep than actual sleep.",
+    background: "url(/assets/image-5.jpg)",
+    circle: "#3e78ed"
+  }
+]
+
